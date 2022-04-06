20b04758af95dd3622e0770870b708689490b705
Salman Nabi <salmannabi183@yahoo.com>
2022-04-06 09:36:55 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// FizzBuzz
+// for(let i = 0; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 == 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 == 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// FizzBuzz using ternary statement
+console.log("FizzBuzz using ternary if/else statement");
+for (let i = 0; i <= 100; i++) {
+    i % 3 == 0 && i % 5 == 0 ? console.log("FizzBuzz") : fizzOrBuzzCheck(i);
+}
+
+function fizzOrBuzzCheck(i) {
+    i % 3 == 0 ? console.log("Fizz") : buzzCheck(i);
+}
+
+function buzzCheck(i) {
+    i % 5 == 0 ? console.log("Buzz") : console.log(i);
+}
